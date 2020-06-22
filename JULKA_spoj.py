da52de982b9f19bf024625ccd8d37095ea9119c2
Sahil Velhal <53365607+sahil-777@users.noreply.github.com>
2020-06-22 18:08:11 +0530

    JULKA - Julka

---
+++ JULKA_spoj.py
@@ -0,0 +1,13 @@
+
+#Problem code : JULKA (from SPOJ)
+#Concept : Ad-hoc.
+#Date : 22/06/2020
+
+
+T=10
+while(T):
+    N=int(input())
+    C=int(input())
+    print((N+C)//2)
+    print((N-C)//2)
+    T-=1
